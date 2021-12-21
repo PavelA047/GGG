914e9c0303626fda52e3e9a76432e4481b3f92d3
pavel <zzzzfogzzzz@gmail.com>
2021-12-22 00:38:12 +0600

    hw_1

---
+++ core/src/com/star/app/ScreenManager.java
@@ -0,0 +1,6 @@
+package com.star.app;
+
+public class ScreenManager {
+    public static final int SCREEN_WIDTH = 1024;
+    public static final int SCREEN_HEIGHT = 576;
+}
---
+++ desktop/src/com/star/app/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.star.app.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.star.app.MyStarGame;
+import com.star.app.ScreenManager;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = ScreenManager.SCREEN_WIDTH;
+        config.height = ScreenManager.SCREEN_HEIGHT;
+        new LwjglApplication(new MyStarGame(), config);
+    }
+}
